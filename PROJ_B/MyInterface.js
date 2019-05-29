deac95451fde96fe79a0fe1b1a61b2c63f4eb4df
João Macedo <mac.j25@gmail.com>
2019-05-29 19:41:36 +0100

    Auto stash before merge of "master" and "origin/master"

--- PROJ_B/MyInterface.js
+++ PROJ_B/MyInterface.js
@@ -15,7 +15,25 @@ class MyInterface extends CGFinterface {
         this.gui = new dat.GUI();
         
         var obj = this;
-
+        
+        this.gui.add(this.scene, 'day_night').name('Toggle day/night').onChange(this.scene.initLights.bind(this.scene));
+        this.initKeys();
         return true;
+    }
+    
+    initKeys() {// create reference from the scene to the GUI
+        this.scene.gui=this;// disable the processKeyboard function
+        this.processKeyboard=function(){};// create a named array to store which keys are being pressed
+        
+        this.activeKeys={};
+    }
+    processKeyDown(event) {// called when a key is pressed down// mark it as active in the array
+        this.activeKeys[event.code]=true;
+    };
+    processKeyUp(event) {// called when a key is released, mark it as inactive in the array
+        this.activeKeys[event.code]=false;
+    };
+    isKeyPressed(keyCode) {// returns true if a key is marked as pressed, false otherwisereturn 
+        this.activeKeys[keyCode] || false;
     }
 }
--- PROJ_B/MyScene.js
+++ PROJ_B/MyScene.js
@@ -21,6 +21,10 @@ class MyScene extends CGFscene
         this.enableTextures(true);
         this.setUpdatePeriod(50);
 
+        
+        //MyInterface
+        this.day_night = true;
+
         //Initialize scene objects
         this.axis = new CGFaxis(this);
         this.plane = new Plane(this, 32);
@@ -34,21 +38,94 @@ class MyScene extends CGFscene
         "images/nose.jpg",
         "images/eye.png",
         "images/white.png");
-        
+        this.amb = new MyCubeMap(this);
+
+        //Initializing Materials
+        this.McubeDay = new CGFappearance(this);
+        this.McubeDay.setAmbient(1, 1, 1, 1);
+        this.McubeDay.setDiffuse(1, 1, 1, 1);
+        this.McubeDay.setSpecular(1, 1, 1, 1);
+        this.McubeDay.setShininess(10);
+        this.McubeDay.loadTexture('images/cubemaptexday.png');
+        this.McubeDay.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.McubeNight = new CGFappearance(this);
+        this.McubeNight.setAmbient(1, 1, 1, 1);
+        this.McubeNight.setDiffuse(1, 1, 1, 1);
+        this.McubeNight.setSpecular(1, 1, 1, 1);
+        this.McubeNight.setShininess(10);
+        this.McubeNight.loadTexture('images/cubemaptexnight.png');
+        this.McubeNight.setTextureWrap('REPEAT', 'REPEAT');
+
         //Objects connected to MyInterface
     }
 
     initLights()
     {
-        this.lights[0].setPosition(15, 2, 5, 1);
-        this.lights[0].setAmbient(0.2, 0.2, 0.2, 1);
-        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
-        this.lights[0].enable();
-        this.lights[0].update();
+        this.setGlobalAmbientLight(0.15, 0.15, 0.15, 1);
+        if(this.day_night)
+        {
+            //SUNLIGHT
+            this.lights[0].setPosition(20, 15, -20, 1);
+            this.lights[0].setAmbient(0.1, 0.1, 0.1, 1);
+            this.lights[0].setDiffuse(1, 1, 1, 1);
+            this.lights[0].setSpecular(1, 1, 1, 1);
+            this.lights[0].enable();
+            this.lights[0].update();
+            this.lights[0].setLinearAttenuation(1);
+
+            this.lights[1].setPosition(20, 15, 20, 0);
+            this.lights[1].setAmbient(0.1, 0.1, 0.1, 1);
+            this.lights[1].setDiffuse(1, 1, 1, 1);
+            this.lights[1].setSpecular(1, 1, 1, 1);
+            this.lights[1].enable();
+            this.lights[1].update();
+            this.lights[1].setLinearAttenuation(1);
+
+        }
+
+        else
+        {
+            //MOONLIGHT
+            this.lights[0].setPosition(20, 15, -20, 1);
+            this.lights[0].setAmbient(0.05, 0.05, 0.05, 1);
+            this.lights[0].setDiffuse(0, 0, 0, 1);
+            this.lights[0].setSpecular(0, 0, 0, 1);
+            this.lights[0].enable();
+            this.lights[0].update();
+            this.lights[0].setLinearAttenuation(1);
+
+            this.lights[1].setPosition(20, 15, 20, 0);
+            this.lights[1].setAmbient(0.05, 0.05, 0.05, 1);
+            this.lights[1].setDiffuse(0, 0, 0, 1);
+            this.lights[1].setSpecular(0, 0, 0, 1);
+            this.lights[1].enable();
+            this.lights[1].update();
+            this.lights[1].setLinearAttenuation(1);
+        }
     }
+
     initCameras()
     {
         this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(45, 45, 45), vec3.fromValues(0, 0, 0));
+    }
+
+    checkKeys(){
+        var text = "Keys pressed: ";
+        var keysPressed=false;
+
+        if (this.gui.isKeyPressed("KeyW")){
+            text+= " W ";
+            keysPressed=true;
+        }
+        
+        if (this.gui.isKeyPressed("KeyS")){
+            text+= " S ";
+            keysPressed=true;
+        }
+        if (keysPressed)
+            console.log(text);
+
     }
 
     setDefaultAppearance()
@@ -58,8 +135,10 @@ class MyScene extends CGFscene
         this.setSpecular(0.2, 0.4, 0.8, 1.0);
         this.setShininess(10.0);
     }
-    
-    update(t){  }
+
+    update(t){
+        this.checkKeys();
+    }
 
     display()
     {
@@ -78,6 +157,10 @@ class MyScene extends CGFscene
         this.axis.display();
         this.setDefaultAppearance();
 
+        //Lights         
+        this.lights[0].update();
+        this.lights[1].update();
+
 
         // ---- BEGIN Primitive drawing section
 
@@ -85,13 +168,20 @@ class MyScene extends CGFscene
         this.pushMatrix();
         //this.rotate(-0.5 * Math.PI, 1, 0, 0);
         this.bird.display();
-        this.popMatrix();
 
         //this.pushMatrix();
         //this.rotate(-0.5*Math.PI, 1, 0, 0);
         //this.scale(10, 10, 10);
         //this.plane.display();
         //this.popMatrix();
+        
+        // CUBE MAP
+        this.popMatrix();
+        this.pushMatrix();
+        this.translate(0, 30, 0);
+        if(this.day_night) this.McubeDay.apply();
+        else this.McubeNight.apply();
+        this.amb.display();
 
 
         // ---- END Primitive drawing section
